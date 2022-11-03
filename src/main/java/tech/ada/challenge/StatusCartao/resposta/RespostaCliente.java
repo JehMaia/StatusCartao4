46d0f3fcd6e0f44acd0d545ff4e8f42e46715e7a
LaísLaur <laislaur@yahoo.com>
2022-11-03 20:03:54 -0300

    versão 2

---
+++ src/main/java/tech/ada/challenge/StatusCartao/resposta/RespostaCliente.java
@@ -0,0 +1,9 @@
+package tech.ada.challenge.StatusCartao.resposta;
+
+import lombok.Data;
+import tech.ada.challenge.StatusCartao.requisicao.RequisicaoCliente;
+
+@Data
+public class RespostaCliente extends RequisicaoCliente {
+    private String identificador;
+}
